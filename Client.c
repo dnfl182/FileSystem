e3ee32a6c4805be5165c4580c24201b486a9a297
Chickening <dnfl182@naver.com>
2016-12-11 23:25:42 +0900

    Revert "ㅅㄱ"
    This reverts commit 3bb9fa2dcd3501f91ab94694ff75f2fe7612f054.

--- Client.c
+++ Client.c
@@ -3,11 +3,36 @@
 #include "FileSystem.h"
 #include "Shall.h"
 
-void testMakeFile(Shall * shall);
 int main(void)
 {
 	Shall *shall = NULL;
 	int i;
-	while(1)
+	for(i = 0; i < 10; i++)
+	{
 		runCommand(&shall);
+		if(i == 0)
+		{
+			testMakeFile(shall);
+		}
+	}
+	
+}
+
+void testMakeFile(Shall * shall)
+{
+	FileSystem * fileSystem = shall -> fileSystem;
+	printf("파일만들기\n"); 
+	Inode * inode = getEmptyInode(fileSystem);
+	char hello[] = "Hello World";
+	char bye[] = "Bye";
+	DataBlock * data = getEmptyDataBlock(fileSystem);
+	DataBlock * data2 = getEmptyDataBlock(fileSystem);
+	pushDataBlockInode(fileSystem , inode , data);
+	pushDataBlockInode(fileSystem , inode , data2);
+	copyBit(data -> data , hello , 0 , 0,strlen(hello) * 8);
+	copyBit(data2 -> data ,bye , 0 , 0,strlen(bye) * 8);
+	printf("Test :getData 4 byte [%s]\n",getDataInode(fileSystem , inode, 0 , 32));
+	printf("Test : [%s]\n",getDataInode(fileSystem , inode, 1024 , 24));
+	//pushFileDirectory(shall , shall -> root , inode , "hell");
+	//pushFileDirectory(shall , shall -> root , inode , "cho");
 }
---
+++ README.md
@@ -0,0 +1,6 @@
+# Funny Project
+
+
+FileSystme : Jeon Byeong Mo
+Shall      : Yang gi chang
+Command    : Lee do hyeon
